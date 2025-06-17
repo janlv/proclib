b1aa0a12e8c5e74cb311e62f88bc7ea3f0bd0b91
Jan Ludvig Vinningland <javi@norceresearch.no>
2025-06-17 15:15:03 +0200

    Refactor __init__.py to include module imports and versioning

--- src/proclib/__init__.py
+++ src/proclib/__init__.py
@@ -1,2 +1,9 @@
-# src/ECLlib/__init__.py
+# src/proclib/__init__.py
 __version__ = "0.1.0"
+
+from .Process import Process
+from .Runner import Runner
+from .Timer import Timer, TimerThread
+
+from psutil import NoSuchProcess
+__all__ = ['Process', 'Runner', 'Timer', 'TimerThread', 'NoSuchProcess']
