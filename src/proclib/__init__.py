049ea7a717a522ed7021d48ddc086b2e24875944
Jan Ludvig Vinningland <javi@norceresearch.no>
2025-06-19 17:08:03 +0200

    Add .gitattributes and install script; update pyproject.toml and __init__.py for versioning

--- src/proclib/__init__.py
+++ src/proclib/__init__.py
@@ -1,5 +1,12 @@
 # src/proclib/__init__.py
-__version__ = "0.1.0"
+
+from ._version import version as __version__
+
+# Importing necessary modules and classes from proclib
+# This allows users to write code like 
+#       from proclib import Process 
+# instead of 
+#       from proclib.Process import Process
 
 from .Process import Process
 from .Runner import Runner
